b082e0fe47332a27d44beac1debac83760929ca4
MOOSA0531 <moosasahib10@gmail.com>
2026-06-03 13:13:48 +0530

    feat: add favourites feature with useReducer and localStorage persistence

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { ThemeProvider } from './context/ThemeContext'
+import { FavouritesProvider } from './context/FavouritesContext'
 import App from './App.jsx'
 import './styles/index.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <ThemeProvider>
-      <App />
+      <FavouritesProvider>
+        <App />
+      </FavouritesProvider>
     </ThemeProvider>
   </StrictMode>,
 )
